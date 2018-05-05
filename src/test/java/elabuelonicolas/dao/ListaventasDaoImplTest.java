bdc5b312e768feac5ccd45a71a6ecc8731997ddc
egremy <egremy.v@gmail.com>
2018-05-05 18:34:54 -0500

    Reslapo resplado peslado respaldo, todo me male sal

---
+++ src/test/java/elabuelonicolas/dao/ListaventasDaoImplTest.java
@@ -0,0 +1,89 @@
+package elabuelonicolas.dao;
+
+import java.util.List;
+import javax.inject.Inject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import elabuelonicolas.bd.domain.Listaventas;
+import elabuelonicolas.dao.listaventas.ListaventasDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/applicationContext.xml" })
+public class ListaventasDaoImplTest {
+	
+	@Inject
+	ListaventasDao listaventasDao;
+
+	@Test
+	public void createListaventa() {
+		try {
+			//System.out.print("\nCreate");
+			Listaventas v = new Listaventas();
+			v.setId(6);
+			v.setIdproducto(12);
+			v.setCantidad(2030);
+			v.setSubtotal(20532.40);
+			
+			listaventasDao.create(v);
+		} catch (Exception e) {
+			System.out.println("Error create test: " + e);
+		}
+	}
+
+	@Test
+	public void readListaventa() {
+		try {
+			Listaventas v = listaventasDao.read(2);
+			System.out.println("\nRead");
+			System.out.println("Id: " + v.getId());
+			System.out.println("Id Prodcuto: " + v.getIdproducto());
+			System.out.println("Cantidad: " + v.getCantidad());
+			System.out.println("Subtotal: " + v.getSubtotal());
+		} catch (Exception e) {
+			System.out.println("Error read test: " + e);
+		}
+	}
+
+	@Test
+	public void updateListaventa() {
+		Listaventas v = listaventasDao.read(1);
+		try {
+			//System.out.print("\nUpdate");
+			v.setId(1);
+			v.setIdproducto(1);
+			v.setCantidad(1);
+			v.setSubtotal(1.10);
+			listaventasDao.update(v);
+		} catch (Exception e) {
+			System.out.println("Error update test: " + e);
+		}
+	}
+
+	@Test
+	public void deleteListaventa() {
+		try {
+			if (listaventasDao.read(9) != null);
+				listaventasDao.delete(9);
+		} catch (Exception e) {
+			System.out.println("Error delete test: " + e);
+		}
+	}
+
+	@Test
+	public void findAllClientes() {
+		try {
+			List<Listaventas> list = listaventasDao.findAll();
+			for (Listaventas v : list) {
+				System.out.println("\nFind All");
+				System.out.println("Id Venta: " + v.getId());
+				System.out.println("Id Producto: " + v.getIdproducto());
+				System.out.println("Cantidad: " + v.getCantidad());
+				System.out.println("Subtotal: " + v.getSubtotal());
+			}
+		} catch (Exception e) {
+			System.out.println("Error findAll: " + e);
+		}
+	}
+}
